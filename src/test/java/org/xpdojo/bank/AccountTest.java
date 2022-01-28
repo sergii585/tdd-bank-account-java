695e4c0e2820c6654c135608a7b3c18a5186825c
sergii585 <sergiy.vernydub@gmil.com>
2022-01-28 13:32:47 +0000

    [JIRA-XXXX]: refactoring

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.xpdojo.bank.Account.initialAccount;
 
 public class AccountTest {
 
     @Test
     public void newAccountShouldHaveBalanceAsZero() {
-        Account account = new Account();
+        Account account = initialAccount();
         assertThat(account.balance()).isEqualTo(0);
     }
 }
