ebe252e8fd897cf1e4b95f6218e828cafea091d2
sergii585 <sergiy.vernydub@gmil.com>
2022-01-28 13:30:19 +0000

    [JIRA-XXXX]: refactoring

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,6 +1,9 @@
 package org.xpdojo.bank;
 
 public class Account {
+    public static Account initialAccount(){
+        return new Account();
+    }
     public int balance(){
         return 0;
     }
